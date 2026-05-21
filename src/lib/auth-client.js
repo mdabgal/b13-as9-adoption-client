8db78c78cbb9540c7124f2559855b86bb5473bfa
Abgal <hmdabgal@gmail.com>
2026-05-21 09:32:07 +0600

    fix: dynamic base url for production

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,18 +1,33 @@
-// import { createAuthClient } from "better-auth/react"
+
+
+
+// import { createAuthClient } from "better-auth/react";
+
 // export const authClient = createAuthClient({
-//     /** The base URL of the server (optional if you're using the same domain) */
-//     baseURL: process.env.BETTER_AUTH_URL
-// })
+//   baseURL: "http://localhost:3000",
+// });
 
-
-// export const { signIn, signUp, useSession } = createAuthClient()
-
-
+// export const {
+//   signIn,
+//   signUp,
+//   useSession,
+//   signOut
+// } = authClient;
 
 import { createAuthClient } from "better-auth/react";
 
+
+const getBaseURL = () => {
+
+  if (typeof window !== "undefined" && window.location.origin.includes("vercel.app")) {
+    return window.location.origin;
+  }
+  
+  return "http://localhost:3000";
+};
+
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: getBaseURL(),
 });
 
 export const {
