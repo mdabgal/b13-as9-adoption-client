c5f70cf65719aeed6a58db3d71709da6f0c9496c
Abgal <hmdabgal@gmail.com>
2026-05-21 09:49:50 +0600

    fix url

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,33 +1,8 @@
-
-
-
-// import { createAuthClient } from "better-auth/react";
-
-// export const authClient = createAuthClient({
-//   baseURL: "http://localhost:3000",
-// });
-
-// export const {
-//   signIn,
-//   signUp,
-//   useSession,
-//   signOut
-// } = authClient;
-
 import { createAuthClient } from "better-auth/react";
 
-
-const getBaseURL = () => {
-
-  if (typeof window !== "undefined" && window.location.origin.includes("vercel.app")) {
-    return window.location.origin;
-  }
-  
-  return "http://localhost:3000";
-};
-
 export const authClient = createAuthClient({
-  baseURL: getBaseURL(),
+ 
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:5000", 
 });
 
 export const {
@@ -36,3 +11,26 @@ export const {
   useSession,
   signOut
 } = authClient;
+
+// import { createAuthClient } from "better-auth/react";
+
+
+// const getBaseURL = () => {
+
+//   if (typeof window !== "undefined" && window.location.origin.includes("vercel.app")) {
+//     return window.location.origin;
+//   }
+  
+//   return "http://localhost:3000";
+// };
+
+// export const authClient = createAuthClient({
+//   baseURL: getBaseURL(),
+// });
+
+// export const {
+//   signIn,
+//   signUp,
+//   useSession,
+//   signOut
+// } = authClient;
